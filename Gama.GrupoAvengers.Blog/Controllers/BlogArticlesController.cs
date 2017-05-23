df569b6e7d5dc4ae215d3a406b341ecfe8aaac1b
Weslley Ribeiro <avengers10gama@gmail.com>
2017-05-23 03:49:30 -0300

    Publicação de artigos

--- Gama.GrupoAvengers.Blog/Controllers/BlogArticlesController.cs
+++ Gama.GrupoAvengers.Blog/Controllers/BlogArticlesController.cs
@@ -25,5 +25,26 @@ namespace Gama.GrupoAvengers.Blog.Controllers
         {
             return View();
         }
+
+        // GET: BlogArticles/Post4
+        public ActionResult Post4()
+        {
+            return View();
+        }
+        // GET: BlogArticles/Post5
+        public ActionResult Post5()
+        {
+            return View();
+        }
+        // GET: BlogArticles/Post6
+        public ActionResult Post6()
+        {
+            return View();
+        }
+        // GET: BlogArticles/Post7
+        public ActionResult Post7()
+        {
+            return View();
+        }
     }
 }
